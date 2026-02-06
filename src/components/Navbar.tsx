2bcf6024f2edcb9707f24afc2acd4a41e2333d76
이강욱 <sangdy85@gmail.com>
2026-02-06 11:13:28 +0900

    Diag: Emergency Neutralize Navbar to stop auth() invocation crash

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,8 @@
 import Link from 'next/link';
 import styles from './Navbar.module.css';
-import { auth, signOut } from "@/auth";
 
 export default async function Navbar() {
-  const session = await auth();
-  const isLoggedIn = !!(session && session.user);
+  const isLoggedIn = false; // Emergency Neutralization
 
   return (
     <nav className={styles.navbar}>
@@ -16,35 +14,15 @@ export default async function Navbar() {
         <div className={styles.links}>
           {isLoggedIn ? (
             <>
-              <Link href="/" className={styles.link}>
-                홈
-              </Link>
-              <Link href="/personal" className={styles.link}>
-                점수 기록
-              </Link>
-              <Link href="/team" className={styles.link}>
-                팀 관리
-              </Link>
-              <Link href="/stats" className={styles.link}>
-                통계/순위
-              </Link>
-              <form action={async () => {
-                'use server';
-                await signOut({ redirectTo: "/" });
-              }}>
-                <button type="submit" className={styles.logoutBtn}>
-                  로그아웃
-                </button>
-              </form>
+              <Link href="/" className={styles.link}>홈</Link>
+              <Link href="/personal" className={styles.link}>점수 기록</Link>
+              <Link href="/team" className={styles.link}>팀 관리</Link>
+              <Link href="/stats" className={styles.link}>통계/순위</Link>
             </>
           ) : (
             <>
-              <Link href="/login" className={styles.link}>
-                로그인
-              </Link>
-              <Link href="/register" className="btn btn-primary">
-                회원가입
-              </Link>
+              <Link href="/login" className={styles.link}>로그인</Link>
+              <Link href="/register" className="btn btn-primary">회원가입</Link>
             </>
           )}
         </div>
