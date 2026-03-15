951816b7b25a9d2f2330f0c669a85b6b2042cbc0
이강욱 <sangdy85@gmail.com>
2026-03-15 16:27:23 +0900

    debug: add detailed header logging for POST requests to diagnose 405 error

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,16 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
     // POST 요청인 경우에만 헤더를 출력합니다 (서버 액션 디버깅용)
     if (request.method === 'POST') {
-        console.log('--- [DEBUG] POST Request Headers ---');
+        console.log('--- [DEBUG] POST Request Info ---');
         console.log('Method:', request.method);
         console.log('URL:', request.url);
+        console.log('Next-Action:', request.headers.get('next-action'));
         console.log('Origin:', request.headers.get('origin'));
         console.log('Host:', request.headers.get('host'));
+        console.log('Referer:', request.headers.get('referer'));
         console.log('X-Forwarded-Proto:', request.headers.get('x-forwarded-proto'));
         console.log('X-Forwarded-Host:', request.headers.get('x-forwarded-host'));
+        console.log('X-Forwarded-For:', request.headers.get('x-forwarded-for'));
         console.log('------------------------------------');
     }
     return NextResponse.next();
