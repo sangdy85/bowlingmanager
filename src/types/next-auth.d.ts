7e8e77c587489b046c64e7da87682c36ae826e38
이강욱 <sangdy85@gmail.com>
2026-03-15 17:46:29 +0900

    fix: add next-auth types and clean up project

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,21 +1,17 @@
-import NextAuth, { DefaultSession } from "next-auth"
-import { JWT } from "next-auth/jwt"
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-    interface Session {
-        user: {
-            id: string
-            role: string
-        } & DefaultSession["user"]
-    }
-
-    interface User {
-        role: string
-    }
+  interface Session {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession["user"];
+  }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT {
-        role: string
-    }
+  interface JWT {
+    id: string;
+    role: string;
+  }
 }
